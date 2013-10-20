c61967060c0234ca5feba99f338aabba6b7ded52
Adrien Brault <adrien.brault@gmail.com>
2013-10-20 15:45:30 -0700

    haaaa!

--- script/init_travis-senv.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "yes" | sudo add-apt-repository ppa:avsm/ppa-testing
-sudo apt-get update -qq
-sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
-export OPAMYES=1
-opam init
-opam install travis-senv
-eval `opam config env`
